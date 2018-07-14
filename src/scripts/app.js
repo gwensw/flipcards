b9bf0cdaab25fbdbffd72436f955727a18625511
gwensw <gwen.simswilliams@gmail.com>
2018-07-14 10:51:53 +0100

    Create sample decks at setup

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,10 @@
-/* eslint-disable */
-import flashcards from 'flashcards';
-/* eslint-enable */
-import test from './examplemodule';
+import flashcards from 'flashcards';  // eslint-disable-line
+import createSampleDecks from './setup';
 import '../styles/main.sass';
 import indexCardIcon from '../assets/index-card-icon.svg';
-
 import testTemplate from '../templates/testrun.handlebars';
 
-document.querySelector('h1').innerHTML = `${test()}`;
+createSampleDecks(); // create sample decks if there aren't any others
+
 document.querySelector('.imagetest').innerHTML = `<img src='${indexCardIcon}'>`;
 document.querySelector('.handlebarstest').innerHTML += testTemplate({ title: 'Hi world', body: 'handlebars here' });
-console.log(flashcards);
