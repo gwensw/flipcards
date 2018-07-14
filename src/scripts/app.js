5339549d1dfbd2f113326b8e7eeecc850389b7d6
gwensw <gwen.simswilliams@gmail.com>
2018-07-14 11:54:13 +0100

    Start building out homepage design

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,10 @@
 import flashcards from 'flashcards';  // eslint-disable-line
 import createSampleDecks from './setup';
 import '../styles/main.sass';
-import indexCardIcon from '../assets/index-card-icon.svg';
-import testTemplate from '../templates/testrun.handlebars';
+// import indexCardIcon from '../assets/index-card-icon.svg';
+// import testTemplate from '../templates/testrun.handlebars';
 
-createSampleDecks(); // create sample decks if there aren't any others
+createSampleDecks(); // create sample decks if homepage is empty
 
-document.querySelector('.imagetest').innerHTML = `<img src='${indexCardIcon}'>`;
-document.querySelector('.handlebarstest').innerHTML += testTemplate({ title: 'Hi world', body: 'handlebars here' });
+// document.querySelector('.imagetest').innerHTML = `<img src='${indexCardIcon}'>`;
+// document.querySelector('.handlebarstest').innerHTML += testTemplate({ title: 'Hi world', body: 'handlebars here' });
--- src/scripts/examplemodule.js
+++
@@ -1,5 +0,0 @@
-function test() {
-  return ('it is all good');
-}
-
-export default test;
